9e7bb70e174c2800e42274abc64adebbbd80daf3
Viet Nguyen <viet@mobilefolk.com>
2023-01-10 21:23:35 +0700

    Add source test RN

---
+++ App.js
@@ -0,0 +1,105 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React, {useEffect} from 'react';
+import type {Node} from 'react';
+import {
+  Alert,
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+import {WebView} from 'react-native-webview';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+/* $FlowFixMe[missing-local-annot] The type annotation(s) required by Flow's
+ * LTI update could not be added via codemod */
+const Section = ({children, title}): Node => {
+  const isDarkMode = useColorScheme() === 'dark';
+  return (
+    <View style={styles.sectionContainer}>
+      <Text
+        style={[
+          styles.sectionTitle,
+          {
+            color: isDarkMode ? Colors.white : Colors.black,
+          },
+        ]}>
+        {title}
+      </Text>
+      <Text
+        style={[
+          styles.sectionDescription,
+          {
+            color: isDarkMode ? Colors.light : Colors.dark,
+          },
+        ]}>
+        {children}
+      </Text>
+    </View>
+  );
+};
+
+const App: () => Node = () => {
+  const webviewRef = React.useRef(null);
+
+  useEffect(() => {
+    checkSupport();
+  }, []);
+
+  const checkSupport = () => {
+    webviewRef.current.injectJavaScript(`
+      if (typeof SharedArrayBuffer === 'undefined') {
+        window.ReactNativeWebView.postMessage('SharedArrayBuffer is not supported')
+      } else {
+        window.ReactNativeWebView.postMessage('SharedArrayBuffer is supported')
+      }
+    `);
+  };
+
+  return (
+    <WebView
+      ref={webviewRef}
+      source={{uri: 'https://mobilefolk.com'}}
+      javaScriptEnabled
+      onMessage={event => alert(JSON.stringify(event.nativeEvent.data))}
+    />
+  );
+};
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
+
+export default App;
